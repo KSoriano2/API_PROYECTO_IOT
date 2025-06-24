1a51a21fcfcec61281a456a60d992151ce812186
KSoriano2 <sorianog500@gmail.com>
2025-06-23 22:16:23 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,16 +1,38 @@
+import express from 'express';
+import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+//importar las rutas
+import infiltradosRoutes from './routes/infiltradosR.js';
+
+//definir los módulos de entrada
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+//definir permisos
+const corsOptions = {
+    origin: '*', //direccion del dominio del servidor
+    methods:['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    Credentials: true
+}
+
+
+const app = express();
 app.use(cors(corsOptions));
-app.use(express.json()); //interpretar los objetos json
-app.use(express.urlencoded({extended:true})) //se añade para aceptar formularios
-app.use('/uploads',express.static(path.join(__dirname,'../uploads')));
+app.use(express.json()); //interpreta objetos json
+app.use(express.urlencoded({extended: true})) //se añade para mostrar formularios
+app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
+//indicar que rutas se utiliza
+app.use('/api', infiltradosRoutes)
 
-// indicar que rutas se utilizan
 
 
-app.use('/api', authRoutes)  //IMPORTACION
-
-app.use((req,resp,next)=>{
+app.use((req, resp, next)=>{
     resp.status(400).json({
-        message:'Endponit noy fount'
+        message: 'Endpoint not found'
     })
 })
+
 export default app;
